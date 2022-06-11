682517427b8acea1b15f92f88a5e1141184d17f4
Tejprakash Upadhyay <93568571+TejprakashUpadhyay@users.noreply.github.com>
2022-06-11 21:04:05 +0530

    init

---
+++ Basic-1/AddTwoNumber.java
@@ -0,0 +1,8 @@
+class AddTwoNumber{
+    public static void main(String[] args){
+        int number1= 23;
+        int number2 = 46;
+        int Sum = number1+number2;
+        System.out.println("Sum of two number "+ Sum);
+    }
+}
